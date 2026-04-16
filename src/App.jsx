fe2508593b3013ee3fb063b72f309c11a77fd4ce
Asad <hafizalasadm@gmail.com>
2026-04-16 19:17:06 +0600

    compose App with BrowserRouter, all Routes, TimelineProvider, and ToastContainer

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { TimelineProvider } from './context/TimelineContext';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import FriendDetail from './pages/FriendDetail';
+import Timeline from './pages/Timeline';
+import Stats from './pages/Stats';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+  return (
+    <TimelineProvider>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/"           element={<Home />} />
+              <Route path="/friend/:id" element={<FriendDetail />} />
+              <Route path="/timeline"   element={<Timeline />} />
+              <Route path="/stats"      element={<Stats />} />
+              <Route path="*"           element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="light"
+        />
+      </BrowserRouter>
+    </TimelineProvider>
+  );
+}
